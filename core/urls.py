ca36955bd04f5db4cf057f72f734ef554ec65e5f
Otabek Narziqulov <otabeknarz@gmail.com>
2024-09-30 12:02:41 +0500

    added /stats/ url

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
+    path("stats/", views.stats),
     path("add/", views.add_people),
     path("add-tg-people/", views.add_tg_people),
     path("get-tg-people/<str:id>/", views.get_tg_people),
